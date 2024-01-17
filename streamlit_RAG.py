eda448da646ded8b09f34e9a4be7701a2859defd
GoDK36 <siinwoo0306@gmail.com>
2024-01-17 22:02:21 +0900

    gemini로 변경

--- streamlit_RAG.py
+++ streamlit_RAG.py
@@ -6,8 +6,8 @@ from loguru import logger
 
 ## 메모리를 가진 체인이 필요
 from langchain.chains import ConversationalRetrievalChain
-## llm은 OpenAI꺼
-from langchain.chat_models import ChatOpenAI
+## llm은 gemini꺼
+from langchain.chat_models import Chatgemini
 
 ## 여러 유형의 문서를 이해하기위한 라이브러리(PDF, DOC, PPT)
 from langchain.document_loaders import PyPDFLoader
@@ -25,14 +25,14 @@ from langchain.memory import ConversationBufferWindowMemory
 from langchain.vectorstores import FAISS
 
 # from streamlit_chat import message
-from langchain.callbacks import get_openai_callback
+from langchain.callbacks import get_gemini_callback
 from langchain.memory import StreamlitChatMessageHistory
 
 
 # 메인 함수
 def main():
     st.set_page_config(
-        page_title="DirChat",
+        page_title="Gemini RAG",
         page_icon=":books:"
     )
 
@@ -47,18 +47,18 @@ def main():
 
     with st.sidebar:
         uploaded_files = st.file_uploader("UPload your file", type=["pdf", 'docx', 'pptx'], accept_multiple_files=True)
-        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+        gemini_api_key = st.text_input("GEMINI API Key", key="chatbot_api_key", type="password")
         process = st.button("Process")
 
     if process:
-        if not openai_api_key:
-            st.info("Please add your OpenAI API key to continue.")
+        if not gemini_api_key:
+            st.info("Please add your gemini API key to continue.")
             st.stop()
         files_text = get_text(uploaded_files)
         text_chunks = get_text_chunks(files_text)
         vectorestore = get_vectorstore(text_chunks)
 
-        st.session_state.conversation = get_conversation_chain(vectorestore, openai_api_key)
+        st.session_state.conversation = get_conversation_chain(vectorestore, gemini_api_key)
 
     if 'messages' not in st.session_state:
         st.session_state['messages'] = [{'role' : 'assistant',
@@ -85,7 +85,7 @@ def main():
 
             with st.spinner("Thinking..."):
                 result = chain({"question": query})
-                with get_openai_callback() as cb:
+                with get_gemini_callback() as cb:
                     st.session_state.chat_history = result['chat_history']
                 response = result['answer']
                 source_documents = result['source_documents']
@@ -155,8 +155,8 @@ def get_vectorstore(text_chunks):
     vectordb = FAISS.from_documents(text_chunks, embeddings)
     return vectordb
 
-def get_conversation_chain(vectorstore, openai_api_key):
-    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name="gpt-3.5-turbo", temperature=0)
+def get_conversation_chain(vectorstore, gemini_api_key):
+    llm = Chatgemini(gemini_api_key=gemini_api_key, model_name="gpt-3.5-turbo", temperature=0)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         chain_type="stuff", 
